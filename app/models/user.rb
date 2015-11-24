d8dec244823c97d58714516ca723dbdd40891b83
StephanieBeaton <stefanybeaton@gmail.com>
2015-11-24 21:39:26 +0000

    missing pieces

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,18 @@ class User < ActiveRecord::Base
   # and password_confirmation to our user model.
   has_secure_password
 
+  validates :email,
+    presence: true
+
+  validates :firstname,
+    presence: true
+
+  validates :lastname,
+    presence: true
+
+  validates :password,
+    length: { in: 6..20 }, on: :create
+
   has_many :reviews
 
   def full_name
