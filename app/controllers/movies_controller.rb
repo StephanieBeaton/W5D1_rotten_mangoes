c80de362fd6dfb73e89bc766eedf02c3093c6a14
StephanieBeaton <stefanybeaton@gmail.com>
2015-11-26 04:33:48 +0000

    refactored movie search and 9. AR scopes

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,10 +1,21 @@
 class MoviesController < ApplicationController
 
   def index
-    # @movies = Movie.all.page(params[:page]).per(10)
-    # @products = Product.order("name").page(params[:page]).per(5)
-    # @movies = Movie.all
-    @movies = Movie.page(params[:page]).per(10)
+
+    byebug
+
+    if params[:search]
+      @movies = Movie.search(params)
+      byebug
+    else
+      @movies = Movie.all
+    end
+
+    # pagination
+    @movies = @movies.page(params[:page]).per(10)
+
+    render :index
+
   end
 
   def show
@@ -45,50 +56,6 @@ class MoviesController < ApplicationController
     redirect_to movies_path
   end
 
-
-  def search
-    # params = {:utf8 =>"✓",
-    #            :title =>"Chicago",
-    #            :commit =>"Search"}
-
-    where_arg = ''
-    if params[:title] != ''
-      where_arg += 'title LIKE "' + "%#{params[:title]}" + '%" '
-    end
-
-    if params[:director] != ''
-      if where_arg != ''
-        where_arg += ' AND '
-      end
-      where_arg += 'director LIKE "' + "%#{params[:director]}" + '%" '
-    end
-
-    if params[:duration] != '0'
-
-      # 1, "Under 90 minutes"
-      # 2, "Between 90 and 120 minutes"
-      # 3, "Over 120 minutes"
-
-      if where_arg != '' && params[:duration] != "0" && params[:duration] != ""
-        where_arg += ' AND '
-      end
-
-      case params[:duration]
-      when "1"
-        where_arg += 'runtime_in_minutes < 90'
-      when "2"
-        where_arg += 'runtime_in_minutes BETWEEN 90 AND 120 '
-      when "3"
-        where_arg += 'runtime_in_minutes > 120 '
-      end
-
-    end
-
-    # User.where("name like :kw or description like :kw", :kw=>"%#{params[:query]}%").to_sql
-
-    @movies = Movie.where(where_arg).page(params[:page]).per(10)
-    render :index
-  end
 
   protected
 
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -29,6 +29,54 @@ class Movie < ActiveRecord::Base
     reviews.sum(:rating_out_of_ten)/reviews.size
   end
 
+  #  Example
+  # scope :created_before, ->(time) { where("created_at < ?", time) }
+
+  scope :title_like, ->(partial_title) { where("title LIKE ?", "%#{partial_title}%") }
+
+  scope :director_like, ->(partial_director) { where("director LIKE ?", "%#{partial_director}%")}
+
+  scope :runtime_less_than, ->(runtime_limit) { where("runtime_in_minutes < ?", runtime_limit)}
+
+  scope :runtime_greater_than, ->(runtime_limit) { where("runtime_in_minutes > ?", runtime_limit)}
+
+  scope :runtime_between, ->(lower_bound, upper_bound) { where("runtime_in_minutes BETWEEN ? AND ?", lower_bound, upper_bound)}
+
+
+  def self.search(params)
+    @movies = Movie.all
+
+    byebug
+    if params[:title] != ''
+      @movies = @movies.title_like(params[:title])
+      byebug
+    end
+
+    if params[:director] != ''
+      @movies = @movies.director_like(params[:director])
+      byebug
+    end
+
+    # 1, "Under 90 minutes"
+    # 2, "Between 90 and 120 minutes"
+    # 3, "Over 120 minutes"
+
+    case params[:duration]
+    when "1"
+      @movies = @movies.runtime_less_than(90)
+      byebug
+    when "2"
+      @movies = @movies.runtime_between(90, 120)
+      byebug
+    when "3"
+      @movies = @movies.runtime_greater_than(120)
+      byebug
+    end
+
+    byebug
+    @movies
+  end
+
   protected
 
   def release_date_is_in_the_past
